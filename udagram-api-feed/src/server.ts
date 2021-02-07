5eaa72ad14110902a52ec12b655f75960a6f6809
MKortmann <companymvms@gmail.com>
2021-02-07 11:02:33 +0100

    [udagra-api-feed] - updating package.json, fix problem

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -19,6 +19,12 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
+  // We need this at our backend to allows everyone to be able to perform
+  // these actions. It allows my computer and my own IP address to be able
+  // to access our s3 bucket without being blocked.
+  /*Now, once our application is actually deployed into Kubernetes, best practice
+  would be that for the allowed origin, we would just specify what resource the Kubernetes
+  resources are running through.*/
   app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
