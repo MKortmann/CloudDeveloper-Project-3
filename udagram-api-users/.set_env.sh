d55b426ad56c3833f924d7a437a62f27c8eeb857
MKortmann <companymvms@gmail.com>
2021-02-11 20:36:39 +0100

    removing local variables

---
+++ udagram-api-users/.set_env.sh
@@ -0,0 +1,28 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+POSTGRES_USERNAME=udagramdev
+POSTGRES_PASSWORD=Samsung2020
+POSTGRES_HOST=udagramdev.cfpseudia1c8.eu-central-1.rds.amazonaws.com
+POSTGRES_DB=udagramdev
+AWS_BUCKET=udagram-073942986821-dev
+AWS_REGION=eu-central-1
+AWS_PROFILE=default
+JWT_SECRET=hello
+URL=http://localhost:8100
+
+
+# export const config = {
+# 			"username": "udagramdev",
+# 			"password": "Samsung2020",
+# 			"database": "udagramdev",
+# 			"host": "udagramdev.cfpseudia1c8.eu-central-1.rds.amazonaws.com",
+# 			"dialect": "postgres",
+# 			"aws_region": "eu-central-1",
+# 			"aws_profile": "default",
+# 			"aws_media_bucket": "udagram-073942986821-dev",
+# 			"version": 'v4',
+# 			"jwt": {
+# 				"secret": "hello"
+# 			},
+# 			"url": 'http://localhost:8100'
+# }
